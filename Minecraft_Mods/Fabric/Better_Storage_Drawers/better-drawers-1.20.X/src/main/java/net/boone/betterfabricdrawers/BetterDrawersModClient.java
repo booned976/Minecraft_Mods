a397dbf2d6303d899f9d8bcd1175e8b98d85ae0c
Rogue Boone <booned976@gmail.com>
2024-03-14 21:01:40 -0600

    commiting origin

---
+++ Minecraft_Mods/Fabric/Better_Storage_Drawers/better-drawers-1.20.X/src/main/java/net/boone/betterfabricdrawers/BetterDrawersModClient.java
@@ -0,0 +1,10 @@
+package net.boone.betterfabricdrawers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterDrawersModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
